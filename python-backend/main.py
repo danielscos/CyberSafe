513457ea3ea82a256d30200b1d6faaac9a01b3ae
danielscos <danielka17.grosso@gmail.com>
2025-05-13 13:29:54 -0400

    feat: enhance hashing endpoints with hash type in responses and add missing endpoints

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile # type: ignore
 from rustlib import (
     hash_sha256_str,
     hash_sha256_bytes,
@@ -16,12 +16,14 @@ app = FastAPI()
 async def root():
     return {"status": "up"}
 
+
+
 @app.get("/hash_sha256")
 async def hash_sha256_endpoint(text: str):
     digest = hash_sha256_str(text)
-    return {"hash": digest}
+    return {"hash": digest, "hash_type": "sha256"}
 
-@app.post("/hash_file")
+@app.post("/hash_file_sha256")
 async def hash_file_endpoint(file: UploadFile = File(...)):
     contents = await file.read()
 
@@ -29,3 +31,50 @@ async def hash_file_endpoint(file: UploadFile = File(...)):
 
     return {"filename": file.filename, "hash_type": "sha256", "hash": digest}
 
+
+
+@app.get("/hash_sha512")
+async def hash_sha512_endpoint(text: str):
+    digest = hash_sha512_str(text)
+    return {"hash": digest, "hash_type": "sha512"}
+
+
+@app.post("/hash_file_sha512")
+async def hash_file_endpoint(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    digest = hash_sha512_bytes(contents)
+
+    return {"filename": file.filename, "hash_type": "sha256", "hash": digest}
+
+
+
+@app.get("/hash_sha1")
+async def hash_sha1_endpoint(text: str):
+    digest = hash_sha1_str(text)
+    return {"hash": digest, "hash_type": "sha1"}
+
+
+@app.post("/hash_file_sha1")
+async def hash_file_endpoint(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    digest = hash_sha1_bytes(contents)
+
+    return {"filename": file.filename, "hash_type": "sha1", "hash": digest}
+
+
+
+@app.get("/hash_md5")
+async def hash_sha1_endpoint(text: str):
+    digest = hash_md5_str(text)
+    return {"hash": digest, "hash_type": "md5"}
+
+
+@app.post("/hash_file_md5")
+async def hash_file_endpoint(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    digest = hash_md5_bytes(contents)
+
+    return {"filename": file.filename, "hash_type": "md5", "hash": digest}
